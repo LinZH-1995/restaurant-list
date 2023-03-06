3bfa25633f1ed93c9d0ba41934cb68cf298358a0
LinZH-1995 <lin84610@gmail.com>
2023-03-06 17:53:49 +0800

    add UI and set route for index also change directory for webfonts folder

--- app.js
+++ app.js
@@ -4,6 +4,14 @@ const restaurantList = require('./restaurant.json')
 const app = express()
 const port = 3000
 
-app.engine('exphbs', exphbs.engine({ defaultLayout: 'main' }))
-app.set('view engine', 'exphbs')
+app.engine('handlebars', exphbs.engine({ defaultLayout: 'main' }))
+app.set('view engine', 'handlebars')
 app.use(express.static('public'))
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Now Server is working on localhost:${port}`)
+})
